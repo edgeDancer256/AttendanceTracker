ba0b67ed2153c2141f12fb848390f77bb3f8a229
edgeDancer256 <luciferstrider@gmail.com>
2022-05-13 12:35:29 +0530

    Welcome Screens

--- app/src/main/java/com/nazgul/attendancetracker/MainActivity.java
+++ app/src/main/java/com/nazgul/attendancetracker/MainActivity.java
@@ -7,9 +7,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.net.Inet4Address;
+
 public class MainActivity extends AppCompatActivity {
 
     Button admin_login;
+    Button teacher_login;
+    Button student_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,24 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent in = new Intent(MainActivity.this, AdminLogin.class);
+                startActivity(in);
+            }
+        });
+
+        teacher_login = (Button) findViewById(R.id.button);
+        teacher_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(MainActivity.this, TeacherLogin.class);
+                startActivity(in);
+            }
+        });
+
+        student_login = (Button) findViewById(R.id.button3);
+        student_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(MainActivity.this, StudentLogin.class);
                 startActivity(in);
             }
         });
