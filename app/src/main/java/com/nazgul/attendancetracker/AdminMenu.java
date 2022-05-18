786648ce842e8c235bd617a73564fe571986f5f3
edgeDancer256 <luciferstrider@gmail.com>
2022-05-18 20:26:58 +0530

    RDS Connection Try

---
+++ app/src/main/java/com/nazgul/attendancetracker/AdminMenu.java
@@ -0,0 +1,71 @@
+package com.nazgul.attendancetracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class AdminMenu extends AppCompatActivity {
+
+    private TextView t1;
+
+    private static final String DATABASE_NAME = "teachers";
+    private static final String url = "jdbc:mysql://attendance-tracker-db.cqqwgqkmnmfd.ap-south-1.rds.amazonaws.com" +
+            DATABASE_NAME;
+    private static final String username = "admin", password = "admin1234";
+    private static final String TABLE_NAME = "bsc";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_menu);
+
+        t1 = findViewById(R.id.textView);
+
+        try {
+            showRecords();
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
+
+        Toast.makeText(this, "Logged in", Toast.LENGTH_SHORT).show();
+    }
+
+    private void showRecords() throws SQLException {
+        new Thread(() -> {
+
+            StringBuilder rec = new StringBuilder();
+
+            try {
+                Class.forName("com.mysql.jdbc.driver");
+                Connection conn = DriverManager.getConnection(url,username, password);
+                Statement st = conn.createStatement();
+
+                ResultSet rs = st.executeQuery("SELECT * FROM " + TABLE_NAME);
+                while (rs.next()) {
+                    rec.append("col1: ").append(rs.getString(1)).append(" col2: ").append(rs.getString(2)).append(" col3: ").append(rs.getString(3)).append("\n");
+                }
+                Log.e("rep", "conn success");
+                conn.close();
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Log.e("stuff", rec.toString());
+                    t1.setText(rec.toString());
+                }
+            });
+        }).start();
+    }
+}
